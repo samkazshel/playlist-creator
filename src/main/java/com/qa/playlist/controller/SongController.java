39bba47e19168aa58592514b166ee461f49e6a46
samkazshel <samsheldon779@gmail.com>
2021-11-22 14:13:38 +0000

    PQ-8 #comment added initial delete method

--- src/main/java/com/qa/playlist/controller/SongController.java
+++ src/main/java/com/qa/playlist/controller/SongController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,5 +47,10 @@ public class SongController {
 		return new ResponseEntity<Song>(this.service.update(id, song), HttpStatus.ACCEPTED);
 	}
 	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Song> delete(@PathVariable Long id){
+		return this.service.delete(id) ? new ResponseEntity<Song>(HttpStatus.NO_CONTENT)
+				:new ResponseEntity<Song>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 	
 }
