b53fad5d8f237c09942f56c66d2ba7469a49db1d
samkazshel <samsheldon779@gmail.com>
2021-11-22 10:09:01 +0000

    PQ-2 #comment adding in playlist consturctor

--- src/main/java/com/qa/playlist/domain/Playlist.java
+++ src/main/java/com/qa/playlist/domain/Playlist.java
@@ -1,5 +1,19 @@
 package com.qa.playlist.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Playlist {
+	@Id
+	@GeneratedValue
+	private Long playlistID;
 
 }
